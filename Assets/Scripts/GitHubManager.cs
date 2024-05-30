b759c198a06a2ffac38a54de3de558cc0517804c
minervatigon <minervatigon@uma.es>
2024-05-30 13:11:26 +0200

    github manager

---
+++ Assets/Scripts/GitHubManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubManager : MonoBehaviour
+{
+    private string gitHubUrl = "https://github.com/Code-Evolution/juego.git"
+
+    public void OpenLink()
+    {
+        Application.OpenURL(gitHubUrl);
+    }
+}
