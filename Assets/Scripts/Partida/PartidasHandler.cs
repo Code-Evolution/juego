c054181eb3ac0f1e785009fda7352ea1ad81d74c
migueel15 <miguelangeldorado10@gmail.com>
2024-06-01 02:01:22 +0200

    format: change formatter (nada importante)

--- Assets/Scripts/Partida/PartidasHandler.cs
+++ Assets/Scripts/Partida/PartidasHandler.cs
@@ -1,32 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PartidasHandler : MonoBehaviour
-{
-  [SerializeField] GameObject menu;
-  [SerializeField] GameObject partidas;
+// Tipos json ------------------------
 
-  private bool partidasMenuActive = false;
+[System.Serializable]
+public class GameSession {
+  public string nombre;
+  public string fecha;
+  public string nivel;
+}
 
-  public void Start()
-  {
-	CrearInstancias();
+[System.Serializable]
+public class GameSessionList {
+  public GameSession[] partidas;
+}
+
+// ----------------------------------
+
+public class PartidasHandler : MonoBehaviour {
+  [SerializeField]
+  GameObject partidas;
+
+  public void Start() { CrearInstancias(); }
+
+  private void CrearInstancias() {
+    // get body game object in partidas and then his child PartidasLayout
+    GameObject partidasLayout = partidas.transform.Find("Body")
+                                    .transform.Find("PartidasLayout")
+                                    .gameObject;
+    CargarInstanciasPartidas(partidasLayout);
   }
 
-  public void TogglePartidasMenu()
-  {
-	partidasMenuActive = !partidasMenuActive;
-	menu.SetActive(!menu.activeSelf);
-	partidas.SetActive(!partidas.activeSelf);
+  public void CargarInstanciasPartidas(GameObject partidasLayout) {
+
+    // Deserialize JSON to GameSessionList object
+    TextAsset jsonFile = Resources.Load<TextAsset>("partidas");
+
+    GameSessionList sessionList =
+        JsonUtility.FromJson<GameSessionList>(jsonFile.text);
+    foreach (var session in sessionList.partidas) {
+      // create a new instance of the prefab
+      GameObject instance =
+          Instantiate(Resources.Load("Partida", typeof(GameObject)))
+              as GameObject;
+      instance.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => {
+        // load the game scene
+        SceneManager.LoadScene("Nivel" + session.nivel);
+        Time.timeScale = 1;
+      });
+      instance.transform.Find("PanelPartida")
+          .transform.Find("tituloPartida")
+          .GetComponent<TMPro.TextMeshProUGUI>()
+          .text = session.nombre;
+      instance.transform.Find("PanelPartida")
+          .transform.Find("fechaPartida")
+          .GetComponent<TMPro.TextMeshProUGUI>()
+          .text = session.fecha;
+      instance.transform.Find("PanelPartida")
+          .transform.Find("nivelPartida")
+          .GetComponent<TMPro.TextMeshProUGUI>()
+          .text += session.nivel;
+
+      // add to parent
+      instance.transform.SetParent(partidasLayout.transform, false);
+    }
   }
 
-  private void CrearInstancias()
-  {
-	// add component CargoPartida
-	CargarPartida cargarPartida = partidas.AddComponent<CargarPartida>();
-	// get body game object in partidas and then his child PartidasLayout
-	GameObject partidasLayout = partidas.transform.Find("Body").transform.Find("PartidasLayout").gameObject;
-	cargarPartida.CargarInstanciasPartidas(partidasLayout);
-  }
+  public void CrearPartida() {}
 }
--- Assets/Scripts/menu/MenuHandler.cs
+++ Assets/Scripts/menu/MenuHandler.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsHandler : MonoBehaviour
-{
-  [SerializeField] private GameObject mainMenu;
-  [SerializeField] private GameObject settingsMenu;
-  public void ToggleSettings()
-  {
-	mainMenu.SetActive(!mainMenu.activeSelf);
-	settingsMenu.SetActive(!settingsMenu.activeSelf);
+public class MenuHandler : MonoBehaviour {
+  [SerializeField]
+  private GameObject mainMenu;
+  [SerializeField]
+  private GameObject settingsMenu;
+  [SerializeField]
+  private GameObject partidasMenu;
+
+  public void ToggleSettings() {
+    mainMenu.SetActive(!mainMenu.activeSelf);
+    settingsMenu.SetActive(!settingsMenu.activeSelf);
   }
+
+  public void TogglePartidasMenu() {
+    mainMenu.SetActive(!mainMenu.activeSelf);
+    partidasMenu.SetActive(!partidasMenu.activeSelf);
+  }
+
+  public void ExitGame() { Application.Quit(); }
 }
